0238ae75d0cfed8eff6786135de6323f8a8889d1
Denis Snitko <denissnitko@gmail.com>
2020-05-25 12:25:54 +0300

    Обновление Footer

--- docs/_js/main.js
+++ docs/_js/main.js
@@ -47,18 +47,6 @@ $(document).ready(function () {
             }
         }
     });
-
-    let triggerTitle = $('[data="item__title"]'); //Выбираем
-
-    triggerTitle.on('click', function () {
-        $(this).next().slideToggle();
-
-        $(this).addClass('footer-content__item-title--active');
-
-        triggerTitle.not(this).removeClass('footer-content__item-title--active');
-
-        triggerTitle.not(this).next().slideUp();
-    })
 });
 
 
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,6 +1,3 @@
-// import "../../blocks/modules/header/header";
 
 import "../../blocks/modules/products/products";
 import "../../blocks/modules/reviews/reviews";
-
-import "../../blocks/modules/footer/footer";
