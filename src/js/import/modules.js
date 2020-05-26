e3ed6ff9180f1ce8e8cf3490def18db4358af323
Denis Snitko <denissnitko@gmail.com>
2020-05-26 18:28:39 +0300

    All products - Done

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,3 +2,5 @@ import "../../blocks/modules/header/header";
 
 import "../../blocks/modules/products/products";
 import "../../blocks/modules/reviews/reviews";
+
+import "../../blocks/modules/aside-menu/aside-menu";
