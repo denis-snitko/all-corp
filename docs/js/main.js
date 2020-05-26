6678b7544013cfc385e78eab1e089cfb89a03359
Denis Snitko <denissnitko@gmail.com>
2020-05-26 15:26:42 +0300

    page-product - All done

--- docs/js/main.js
+++ docs/js/main.js
@@ -71,6 +71,27 @@ $(document).ready(function () {
 
         return false;
     });
+
+    $(".owl-suggestion").owlCarousel({
+        loop: true,
+        margin: 28,
+        nav: true,
+        navText: [""],
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            }
+        }
+    });
 });
 
 
--- src/blocks/pages/page-product/page-product.js
+++ src/blocks/pages/page-product/page-product.js
@@ -14,4 +14,25 @@ $(function () {
 
         return false;
     });
+
+    $(".owl-suggestion").owlCarousel({
+        loop: true,
+        margin: 28,
+        nav: true,
+        navText: [""],
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            }
+        }
+    });
 });
